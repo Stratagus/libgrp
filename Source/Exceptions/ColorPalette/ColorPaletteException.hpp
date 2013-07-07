17afb2e3e66bac37a2ae87f99fc7ce16ecdfa599
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2013-07-07 15:13:18 -0700

    *Started to implement Exception classes

---
+++ Source/Exceptions/ColorPalette/ColorPaletteException.hpp
@@ -0,0 +1,19 @@
+#ifndef ColorPaletteException_H
+#define ColorPaletteException_H
+
+#include "../GRPException.hpp"
+#include <exception>
+#include <string>
+
+//!The base exception for which all GrpImage exceptions are based off of
+/*!A base exception class
+ * \pre NA
+ * \post NA
+ * \note */
+
+class ColorPaletteException : public GRPException
+{
+};
+
+
+#endif
--- Source/Exceptions/GRPException.hpp
+++ Source/Exceptions/GRPException.hpp
@@ -2,15 +2,41 @@
 #define grpexception_H
 
 #include <exception>
+#include <string>
 
-class GRPFileExceptions : public std::exception
+//!The base exception for which all GrpImage exceptions are based off of
+/*!A base exception class
+ * \pre NA
+ * \post NA
+ * \note */
+
+class GRPException : public virtual std::exception
 {
     
 public:
+    void SetErrorMessage(const std::string &errorMessage);
+    
+    ~GRPException() throw() {}
+    virtual std::string GetErrorMessage();
 protected:
+       std::string humanReadableError;
 private:
     
 };
 
+//!An exception for filesystem based errors
+/*!A Exception
+ * \pre NA
+ * \post NA
+ * \note */
+class GRPImageFilesystemException : public GRPException
+{
+public:
+    void SetFilePath(const std::string &FilePath);
+    std::string GetFilePath();
+protected:
+    std::string *problemPath;
+    
+};
 
 #endif
